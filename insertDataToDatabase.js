55425fbe57fc288c615d56ebb0fdc7d6917df6ed
latedude2 <simasc10@gmail.com>
2021-03-11 15:54:31 +0100

    Small change

--- insertDataToDatabase.js
+++ insertDataToDatabase.js
@@ -22,12 +22,13 @@ run().catch(console.dir);
 function insertData(message)
 {
   window.alert("Inserting data into database");  
+  //Run script from backend - we cannot just run The run(message) here because this file is hosted in github and we need to send a request or sth to the backend website
   run(message);
 }
 
-function addToGlobalScope(functionToAdd)
+function addToGlobalScope()
 {
-  window.insertData = functionToAdd;
+  window.insertData = insertData;
   window.alert("Added test to global scope");    
 }
 
