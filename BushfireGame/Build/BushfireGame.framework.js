7e3d0ad1304169cc3f27cb1d5865e21aac2586e4
latedude2 <simasc10@gmail.com>
2021-03-23 15:34:17 +0100

    New build

--- BushfireGame/Build/BushfireGame.framework.js
+++ BushfireGame/Build/BushfireGame.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2622368;
+STATICTOP = STATIC_BASE + 2628048;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3227,7 +3227,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2622368;
+var STATIC_BUMP = 2628048;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15311,8 +15311,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 55907;
-Module["wasmMaxTableSize"] = 55907;
+Module["wasmTableSize"] = 55939;
+Module["wasmMaxTableSize"] = 55939;
 function invoke_dd(index, a1) {
  var sp = stackSave();
  try {
