d98501fb37c3b5c0f7640e42510e35b870d6b2e8
latedude2 <simasc10@gmail.com>
2021-03-27 14:34:31 +0100

    Try different post receive in backend

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var http = require('http');
-var server = http.createServer ( function(request,response){
+var qs = require('querystring');
 
+
+var server = http.createServer ( function(request,response){
+/*
 response.writeHead(200,{"Content-Type":"text\plain"});
 if(request.method == "GET")
     {
@@ -15,7 +18,30 @@ else
     {
         response.end("Undefined request .");
     }
+    */
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1000000)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            var post = qs.parse(body);
+            console.log(post.say);
+            console.log(post.to);
+        });
+    }
 });
 
- 
+
+
+function (request, response) {
+    
+}
 server.listen(process.env.PORT || 5000);
