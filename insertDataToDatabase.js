164cd5c363f70e7c267b0956fd4246c1cd381e3c
latedude2 <simasc10@gmail.com>
2021-03-27 15:29:59 +0100

    Send data to backend from unity

--- insertDataToDatabase.js
+++ insertDataToDatabase.js
@@ -1,17 +1,17 @@
 function insertData(message)
 {
-  window.alert("Inserting data into database");  
+  window.alert("Inserting data into database: " + message); 
+    
   const url = "https://coopgame.herokuapp.com/app.js";
-  const data = {say: "hi", to:"mom"}
-
+  const data = {say: "sent", to: message}
   $.post(url,data, function(data, status){
     console.log("${data} and status is ${status}")
-  });
+  }); 
 }
 
 function addToGlobalScope()
 {
   window.insertData = insertData;
-  window.alert("Added test to global scope");    
+  window.alert("Added test to global scope"); 
 }
 
