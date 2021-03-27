057bc77f34d1600ec20713316a5ac85f2d9d3565
latedude2 <simasc10@gmail.com>
2021-03-27 15:04:45 +0100

    small change

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var qs = require('querystring');
 
 
 var server = http.createServer ( function(request,response){
-
-    */
     if (request.method == 'POST') {
         var body = '';
 
@@ -12,8 +10,7 @@ var server = http.createServer ( function(request,response){
             body += data;
 
             // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1000000)
+            if (body.length > 10000000)
                 request.connection.destroy();
         });
 
