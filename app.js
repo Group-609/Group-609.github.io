2137ca55671022fc13f028be8279e19493eb67b1
latedude2 <simasc10@gmail.com>
2021-04-01 16:24:04 +0200

    revert last change

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var server = http.createServer ( function(request,response){
       {
         console.log("Sending response: DDA");
         var jsonResponse = {"condition":"DDA"};
-        response.write(request.query.callback + '('+ JSON.stringify(jsonResponse) + ');');
+        response.write(JSON.stringify(jsonResponse));
         response.end();
         //response.end(jsonResponse);
       }
@@ -28,7 +28,7 @@ var server = http.createServer ( function(request,response){
       {
         console.log("Sending response: Control");
         var jsonResponse = {"condition":"Control"};
-        response.write(request.query.callback + '('+ JSON.stringify(jsonResponse) + ');');
+        response.write(JSON.stringify(jsonResponse));
         response.end();
       }
     });
