3d4bd049cf622365eeb93dfd583b881a7a77641f
latedude2 <simasc10@gmail.com>
2021-04-01 15:05:43 +0200

    remove callback stuff

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var server = http.createServer ( function(request,response){
       {
         console.log("Sending response: Control");
         var jsonResponse = {"condition":"Control"};
-        response.write(request.query.callback + '('+ JSON.stringify(jsonResponse) + ');');
+        response.write(JSON.stringify(jsonResponse));
         response.end();
       }
     });
