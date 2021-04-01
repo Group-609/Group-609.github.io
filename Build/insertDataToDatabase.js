e09c7f6a9ce44ad3b77f3f9fdf93e2d4b4949001
latedude2 <simasc10@gmail.com>
2021-04-01 16:34:20 +0200

    new build

--- Build/insertDataToDatabase.js
+++ Build/insertDataToDatabase.js
@@ -32,9 +32,25 @@ function getCondition()
       console.log(JSON.stringify(data));
       alert(JSON.stringify(object));
     },
-    error:function(err){
-      alert(err);
-    } 
+    error: function (jqXHR, exception) {
+      var msg = '';
+      if (jqXHR.status === 0) {
+          msg = 'Not connect.\n Verify Network.';
+      } else if (jqXHR.status == 404) {
+          msg = 'Requested page not found. [404]';
+      } else if (jqXHR.status == 500) {
+          msg = 'Internal Server Error [500].';
+      } else if (exception === 'parsererror') {
+          msg = 'Requested JSON parse failed.';
+      } else if (exception === 'timeout') {
+          msg = 'Time out error.';
+      } else if (exception === 'abort') {
+          msg = 'Ajax request aborted.';
+      } else {
+          msg = 'Uncaught Error.\n' + jqXHR.responseText;
+      }
+      console.log(msg);
+  }, 
   })
   /*
   $.get(url, function( data ) {
