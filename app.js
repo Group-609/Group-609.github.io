03cf86cbd8dd2dfb0db1a3cc06b62a6fb7a3ef50
latedude2 <simasc10@gmail.com>
2021-04-01 16:44:58 +0200

    Add function wrap to json responce

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var server = http.createServer ( function(request,response){
       {
         console.log("Sending response: DDA");
         var jsonResponse = {"condition":"DDA"};
-        response.write(JSON.stringify(jsonResponse));
+        response.write("condition(" + JSON.stringify(jsonResponse) + ");");
         response.end();
         //response.end(jsonResponse);
       }
