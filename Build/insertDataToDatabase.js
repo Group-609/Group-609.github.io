65bd0d97f009788cd1a4f308c965bc8f947f2ba2
latedude2 <simasc10@gmail.com>
2021-04-01 14:00:44 +0200

    New build

--- Build/insertDataToDatabase.js
+++ Build/insertDataToDatabase.js
@@ -9,9 +9,21 @@ function insertData(message)
 }
 function getCondition()
 {
+  $.ajax({
+    url:"https://coopgame.herokuapp.com/app.js",
+    dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+    success:function(json){
+        alert("Success: " + json.condition);
+    },
+    error:function(){
+        alert("Error");
+    }      
+  });
+  /*
   const url = "https://cors.io/?https://coopgame.herokuapp.com/app.js";  //CORS lets us do requests over different web pages
   $.get(url, function( data ) {
     $(".result").html( data );
     alert( "Going to load the following condition:." + condition);
   }, "text");
+  */
 }
