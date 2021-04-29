8f9bb6a055ba778739b0273f50a438878e555aee
Dovile <43847677+dovii@users.noreply.github.com>
2021-04-29 16:01:56 +0200

    show different parts of test feature is now only controllable with test buttons

--- showPartsOfTest.js
+++ showPartsOfTest.js
@@ -2,11 +2,17 @@ const hideShowTime = 500;
 var game1Data;
 var game2Data;
 
+
+window.document.addEventListener('showConsent', handleEventShowConsent, false);
+window.document.addEventListener('showFirstForm', handleEventShowFirstForm, false);
+
 window.document.addEventListener('showGame', handleEventShowGame, false);
 window.document.addEventListener('showSecondForm', handleEventShowSecondForm, false);
 window.document.addEventListener('showGameAgain', handleEventShowGameAgain, false);
 window.document.addEventListener('showLastForm', handleEventShowLastForm, false);
 
+$(".consent-form").hide();
+$(".first-form").hide();
 $(".second-form").hide(); 
 $("#html_embed_widget_11018").hide();  //Unity window
 $(".third-form").hide();  
@@ -15,6 +21,19 @@ $("#thanks_for_participating").hide();
 $("#error_uploading").hide(); 
 
 
+function handleEventShowConsent(e) {
+  $("#html_embed_widget_11018").hide(hideShowTime);  //Unity window
+  $(".consent-form").show(hideShowTime);
+  $(".info-button").hide(hideShowTime);
+  console.log(e.detail);
+}
+
+function handleEventShowFirstForm(e) {
+  $("#html_embed_widget_11018").hide(hideShowTime);  //Unity window
+  $(".first-form").show(hideShowTime);
+  $(".consent-button").hide(hideShowTime);
+  console.log(e.detail);
+}
 
 function handleEventShowGame(e) {
   $("#html_embed_widget_11018").show(hideShowTime);  //Unity window
@@ -44,6 +63,19 @@ function handleEventShowLastForm(e) {
 
 
 //For testing
+$("#showConsent").click(function () {
+  var data = { foo: 'showing consent form' }
+  var event = new CustomEvent('showConsent', { detail: data })
+  window.document.dispatchEvent(event);
+});
+
+$("#showFirstForm").click(function () {
+  var data = { foo: 'showing demographic form' }
+  var event = new CustomEvent('showFirstForm', { detail: data })
+  window.document.dispatchEvent(event);
+});
+
+
 $("#showGame").click(function(){
     var data = { foo: 'showing game' }
     var event = new CustomEvent('showGame', { detail: data })
