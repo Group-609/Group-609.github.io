e72fb43cb9f040d306cf1c16c0a4af74e03a847a
latedude2 <simasc10@gmail.com>
2021-05-02 17:44:49 +0200

    New build

--- Build/Build/Build.framework.js
+++ Build/Build/Build.framework.js
@@ -1309,7 +1309,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3437552;
+STATICTOP = STATIC_BASE + 3438032;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3239,7 +3239,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3437552;
+var STATIC_BUMP = 3438032;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -16385,8 +16385,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 97632;
-Module["wasmMaxTableSize"] = 97632;
+Module["wasmTableSize"] = 97628;
+Module["wasmMaxTableSize"] = 97628;
 function invoke_dd(index, a1) {
  var sp = stackSave();
  try {
